e3c5f389d831c092d68744be2c4f214e2017e902
Guren <237576186+gurendev@users.noreply.github.com>
2025-10-24 09:36:00 -0500

    Get random computer choice

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
-console.log("Hello world");
+const getComputerChoice = () => {
+    const randomNumber = Math.floor(Math.random() * 3);
+    if (randomNumber === 0) {
+        return "rock";
+    } else if (randomNumber === 1) {
+        return "paper";
+    } else {
+        return "scissors";
+    }
+}
+
