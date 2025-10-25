b2e3f8e5fabaee1c9afc7e0eefb79425fd53b848
Guren <237576186+gurendev@users.noreply.github.com>
2025-10-25 09:48:46 -0500

    Add logic to play single round

--- script.js
+++ script.js
@@ -1,3 +1,7 @@
+let humanScore = 0;
+let computerScore = 0;
+const choices = ["rock", "paper", "scissors", "rock", "paper"]
+
 const getComputerChoice = () => {
     const randomNumber = Math.floor(Math.random() * 3);
     if (randomNumber === 0) {
@@ -12,8 +16,21 @@ const getComputerChoice = () => {
 const getHumanChoice = () => {
     let choice = "";
     do {
-        choice = prompt("rock, paper or scissors: ");
+        choice = prompt("rock, paper or scissors: ").toLocaleLowerCase();
     } while (choice !== "rock" && choice !== "paper" && choice !== "scissors");
     return choice;
 }
 
+const playRound = (humanChoice, computerChoice) => {
+    humanIndex = choices.indexOf(humanChoice);
+    computerIndex = choices.indexOf(computerChoice);
+    if (humanIndex === computerIndex) {
+        console.log("Draw!");
+    } else if (choices[humanIndex + 1] === computerChoice) {
+        console.log(`Computer wins with ${computerChoice} vs human's ${humanChoice}`);
+    } else {
+        console.log(`Human wins with ${humanChoice} vs computer's ${computerChoice}`);
+    }
+}
+
+playRound(getHumanChoice(), getComputerChoice());
